9bd3bba757cc5cec1be0a28a3c1a61fee57d6e53
mattak <mattak.me@gmail.com>
2018-07-25 16:51:42 +0900

    :gem: feat/#1 Test Model & UseCase

---
+++ Assets/Tests/EditMode/PagerUseCaseTest.cs
@@ -0,0 +1,42 @@
+﻿using CAFU.Pager.Domain.UseCase;
+using NUnit.Framework;
+
+namespace EditModeTest.CAFU.Pager.Domain.UseCase
+{
+    public class PagerUseCaseTest
+    {
+        [Test]
+        public void GoTest()
+        {
+            var usecase = new PagerUseCase.Factory().Create(0, 5, 30);
+
+            Assert.IsTrue(usecase.HasNext);
+            Assert.IsFalse(usecase.HasPrevious);
+
+            usecase.GoToNext();
+            Assert.IsTrue(usecase.HasNext);
+            Assert.IsTrue(usecase.HasPrevious);
+
+            usecase.GoToNext();
+            usecase.GoToNext();
+            usecase.GoToNext();
+            usecase.GoToNext();
+
+            Assert.IsFalse(usecase.HasNext);
+            Assert.IsTrue(usecase.HasPrevious);
+
+            usecase.GoToPrevious();
+            Assert.IsTrue(usecase.HasNext);
+            Assert.IsTrue(usecase.HasPrevious);
+
+            usecase.GoToPrevious();
+            usecase.GoToPrevious();
+            usecase.GoToPrevious();
+            usecase.GoToPrevious();
+            usecase.GoToPrevious();
+
+            Assert.IsTrue(usecase.HasNext);
+            Assert.IsFalse(usecase.HasPrevious);
+        }
+    }
+}
