89cb45357301196cf8d6b08626a2b8404fa74c99
mattak <mattak.me@gmail.com>
2018-07-25 18:39:47 +0900

    :art: feat/#3 Create Pager sample scene

---
+++ Assets/Examples/Scripts/Presentation/View/Controller.cs
@@ -0,0 +1,9 @@
+﻿using CAFU.Core.Presentation.View;
+using Example.CAFU.Pager.Presentation.Presenter;
+
+namespace Example.CAFU.Stopwatch.Presentation.View
+{
+    public class Controller : Controller<Controller, SamplePresenter, SamplePresenter.Factory>
+    {
+    }
+}
---
+++ Assets/Examples/Scripts/Presentation/View/TextsRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using CAFU.Core.Presentation.View;
+using CAFU.Pager.Presentation.Presenter;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Example.CAFU.Pager.Presentation.View
+{
+    public class TextsRenderer : MonoBehaviour, IView
+    {
+        public Text[] Cells;
+
+        private readonly string[] Data = Enumerable.Range(1, 29).Select(it => it.ToString()).ToArray();
+
+        public void Start()
+        {
+            this.GetPresenter<IPagerPresenter>()
+                .GetPagerOffsetAsObservable()
+                .Subscribe(this.Render)
+                .AddTo(this);
+        }
+
+        private void Render(int offset)
+        {
+            for (int i = 0; i < this.Cells.Length; i++)
+            {
+                if (i + offset >= this.Data.Length - 1)
+                {
+                    this.Cells[i].enabled = false;
+                }
+                else
+                {
+                    this.Cells[i].enabled = true;
+                    this.Cells[i].text = this.Data[offset + i];
+                }
+            }
+        }
+    }
+}
